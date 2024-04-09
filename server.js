7e5cd576aec912ce087068f6a226139b1dde80cc
Khakachang <khakachang22@gmail.com>
2024-04-09 08:02:42 +0530

    UI Done

--- server.js
+++ server.js
@@ -4,19 +4,34 @@ const app = express();
 const path = require("path");
 const port = 8000;
 
-//Set EJS as the view engine
+// Set EJS as the view engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-//Serve static file from the public directory
+// Serve static files from the public directory
 app.use(express.static("public"));
 
-//Define a route to render the index.ejs template
+// Define routes
+
+// Render the index.ejs template for the root URL
 app.get("/", (req, res) => {
     res.render("index");
 });
 
-//Start the server
+// Render the signIn.ejs template for the /signIn route
+app.get("/signIn", (req, res) => {
+    res.render("signIn");
+});
+// Render the register.ejs template for the /Register route
+app.get("/Register", (req, res) => {
+    res.render("register");
+});
+// Render the post.ejs template for the /Register route
+app.get("/Post", (req, res) => {
+    res.render("post");
+});
+
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
